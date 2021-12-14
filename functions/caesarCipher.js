b654a81c25c9bd68d31f7be2bed12261d4a9aab0
Alejandro <aariasmartijena@gmail.com>
2021-12-14 15:18:49 -0400

    write caesarCipher test and algorithm

--- functions/caesarCipher.js
+++ functions/caesarCipher.js
@@ -1,5 +1,21 @@
-function caesarCipher() {
+function caesarCipher(string) {
+  const cipheredString = [];
 
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < string.length; i++) {
+    if ((string.charCodeAt(i) > 64 && string.charCodeAt(i) < 91)
+    || (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123)) {
+      if (string.charCodeAt(i) === 90 || string.charCodeAt(i) === 122) {
+        cipheredString[i] = String.fromCharCode(string.charCodeAt(i) - 25);
+      } else {
+        cipheredString[i] = String.fromCharCode(string.charCodeAt(i) + 1);
+      }
+    } else {
+      cipheredString[i] = string[i];
+    }
+  }
+
+  return cipheredString.join('');
 }
 
 module.exports = caesarCipher;
---
+++ tests/caesarCipher.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const caesarCipher = require('../functions/caesarCipher');
+
+describe('', () => {
+  test('', () => {
+    expect(caesarCipher('the quick brown fox jumps over the lazy dog')).toBe('uif rvjdl cspxo gpy kvnqt pwfs uif mbaz eph');
+  });
+});
