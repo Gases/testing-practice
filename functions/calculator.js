5c79bc4d0964a9ec2d7c422ee2ea72c0aef18531
Alejandro <aariasmartijena@gmail.com>
2021-12-14 13:52:05 -0400

    delete index.test.js

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,5 +1,18 @@
-function calculator() {
-
-}
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    if (b === 0) return 'Cannot divide by 0';
+    if (!a % b === 0) return parseFloat((a / b).toFixed(2));
+    return a / b;
+  },
+};
 
 module.exports = calculator;
--- functions/reverseString.js
+++ functions/reverseString.js
@@ -1,5 +1,12 @@
-function reverseString() {
+/* eslint-disable no-plusplus */
+function reverseString(string) {
+  const reversedString = [];
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString.push(string[i]);
+  }
+
+  return reversedString.join('');
 }
 
 module.exports = reverseString;
