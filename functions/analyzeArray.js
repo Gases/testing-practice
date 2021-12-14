5e89388951e8d39e46a73f01b33864d1310f4bc3
Alejandro <aariasmartijena@gmail.com>
2021-12-14 19:14:10 -0400

    add tests and code for analyzeArray exercise

--- functions/analyzeArray.js
+++ functions/analyzeArray.js
@@ -1,5 +1,29 @@
-function analyzeArray() {
+function analyzeArray(array) {
+  if (array.length === 0) {
+    return {
+      average: undefined,
+      min: undefined,
+      max: undefined,
+      length: 0,
+    };
+  }
 
+  const avg = () => {
+    let counter = 0;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < array.length; i++) {
+      counter += array[i];
+    }
+    if (counter % array.length === 0) return counter / array.length;
+    return parseFloat((counter / array.length).toFixed(3));
+  };
+
+  return {
+    average: avg(),
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length,
+  };
 }
 
 module.exports = analyzeArray;
