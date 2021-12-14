6df7d94efa46856a7a6932c1504f696cc5c94d99
Alejandro <aariasmartijena@gmail.com>
2021-12-14 11:42:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const capitalize = require('./functions/capitalize');
+const reverseString = require('./functions/reverseString');
+const calculator = require('./functions/calculator');
+const caesarCipher = require('./functions/caesarCipher');
+const analyzeArray = require('./functions/analyzeArray');
---
+++ index.test.js
@@ -0,0 +1,1 @@
+const tests = require('./index');
